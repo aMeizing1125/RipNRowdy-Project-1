8f9d67695bb4872694dee8e0b551a0adbf821b67
tyler <tylercedge@gmail.com>
2019-03-09 10:42:38 -0500

    linked firebase and fixed js file link

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBtfleJjantBqTZXzSJnvMJGU6_pMAonPY",
+    authDomain: "bar-crawl-project-f4740.firebaseapp.com",
+    databaseURL: "https://bar-crawl-project-f4740.firebaseio.com",
+    projectId: "bar-crawl-project-f4740",
+    storageBucket: "bar-crawl-project-f4740.appspot.com",
+    messagingSenderId: "917856200360"
+  };
+  firebase.initializeApp(config);
