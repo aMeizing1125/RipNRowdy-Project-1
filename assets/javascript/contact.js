4d3e9815002a412967e054bb86ea0d1e4b2abc66
unknown <hampton.muriel@gmail.com>
2019-03-16 10:00:58 -0400

    final

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -3,13 +3,14 @@
 // Initialize Firebase
 // Mei's Bar Crawl Project config for contact form entry stuff. 
 // One way information
+
 var config = {
-  apiKey: "AIzaSyB76RPsQKnl5tyOVAR1GA5cUxG6KM0t-Xc",
-  authDomain: "bar-crawl-contact-db.firebaseapp.com",
-  databaseURL: "https://bar-crawl-contact-db.firebaseio.com",
-  projectId: "bar-crawl-contact-db",
-  storageBucket: "bar-crawl-contact-db.appspot.com",
-  messagingSenderId: "376710116624"
+  apiKey: "AIzaSyBtfleJjantBqTZXzSJnvMJGU6_pMAonPY",
+  authDomain: "bar-crawl-project-f4740.firebaseapp.com",
+  databaseURL: "https://bar-crawl-project-f4740.firebaseio.com",
+  projectId: "bar-crawl-project-f4740",
+  storageBucket: "bar-crawl-project-f4740.appspot.com",
+  messagingSenderId: "917856200360"
 };
 firebase.initializeApp(config);
 var database = firebase.database();
@@ -17,20 +18,19 @@ var database = firebase.database();
 // ----- GLOBAL VARIABLES ----- //
 
 
-$('#submit').on("click", function() {
-  var customer = {
-    name:  $('#formName').val().trim(),
-    emailAddress: $('#formEmail').val().trim(),
-    brewery: $('#formBrewery').val().trim(),
-    story: $('#formStory').val().trim(),
+$('#submit').on("click", function () {
+    var customer = {
+      name: $('#formName').val().trim(),
+      emailAddress: $('#formEmail').val().trim(),
+      brewery: $('#formBrewery').val().trim(),
+      story: $('#formStory').val().trim(),
+    }
+    database.ref().push(customer);
   }
-  database.ref().push(customer);
-  }
-  
+
 )
 
 //testing because i set database up so you couldn't read or write it. 
 // database.ref().on('value', function (snapshot) {
 // console.log(snapshot);
 // })
-
